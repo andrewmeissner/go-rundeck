d25e2dc26f583c011f79bee3d502e7dd44364344
Andrew Meissner <andrew.meissner@careerbuilder.com>
2018-07-13 23:48:05 -0600

    adding count param to paging into

--- paging.go
+++ paging.go
@@ -2,6 +2,7 @@ package rundeck
 
 // PagingInfo contains information relating to a paginated response
 type PagingInfo struct {
+	Count  int `json:"count"`
 	Total  int `json:"total"`
 	Max    int `json:"max"`
 	Offset int `json:"offset"`
