b4a975565fba535bfc3df58fb7f40069696de24a
Andrew Meissner <andrew.meissner@careerbuilder.com>
2018-07-13 23:49:10 -0600

    adding run and retry funcs

--- jobs.go
+++ jobs.go
@@ -1,11 +1,22 @@
 package rundeck
 
 import (
+	"bytes"
 	"encoding/json"
+	"io"
 	"net/http"
 	"net/url"
 	"strconv"
 	"strings"
+	"time"
+)
+
+const (
+	JobLogLevelDebug   = "DEBUG"
+	JobLogLevelVerbose = "VERBOSE"
+	JobLogLevelInfo    = "INFO"
+	JobLogLevelWarn    = "WARN"
+	JobLogLevelError   = "ERROR"
 )
 
 // Job is information about a Rundeck job
@@ -37,6 +48,24 @@ type ListJobsInput struct {
 	ServerNodeUUIDFilter string
 }
 
+// RunJobInput are the optional paramenters passed when running a job
+type RunJobInput struct {
+	LogLevel string `json:"loglevel,omiempty"`
+	AsUser   string `json:"asUser,omitempty"`
+	// TODO: NodeFilters
+	// TODO: Filter
+	RunAtTime time.Time         `json:"runAtTime,omitempty"`
+	Options   map[string]string `json:"options,omitempty"`
+}
+
+// RetryJobInput are the optional parameters passed when retrying a job based on execution id
+type RetryJobInput struct {
+	LogLevel    string            `json:"loglevel,omitempty"`
+	AsUser      string            `json:"asUser,omitempty"`
+	Options     map[string]string `json:"options,omitempty"`
+	FailedNodes bool              `json:"failedNodes,omitempty"`
+}
+
 // Jobs is information pertaining to jobs API endpoints
 type Jobs struct {
 	c *Client
@@ -66,6 +95,61 @@ func (j *Jobs) List(project string, input *ListJobsInput) ([]*Job, error) {
 
 	var jobs []*Job
 	return jobs, json.NewDecoder(res.Body).Decode(&jobs)
+}
+
+// Run will execute a job
+func (j *Jobs) Run(jobID string, input *RunJobInput) (*Execution, error) {
+	uri := j.c.RundeckAddr + "/job/" + jobID + "/run"
+
+	var body io.Reader
+	if input != nil {
+		bs, err := json.Marshal(input)
+		if err != nil {
+			return nil, err
+		}
+		body = bytes.NewReader(bs)
+	}
+
+	res, err := j.c.post(uri, body)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, makeError(res.Body)
+	}
+
+	var execution Execution
+	return &execution, json.NewDecoder(res.Body).Decode(&execution)
+}
+
+// Retry retries a job based on an execution id
+func (j *Jobs) Retry(jobID string, execID int64, input *RetryJobInput) (*Execution, error) {
+	uri := j.c.RundeckAddr + "/job/" + jobID + "/retry/" + strconv.FormatInt(execID, 10)
+
+	var body io.Reader
+	if input != nil {
+		bs, err := json.Marshal(input)
+		if err != nil {
+			return nil, err
+		}
+		body = bytes.NewReader(bs)
+	}
+
+	res, err := j.c.post(uri, body)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, makeError(res.Body)
+	}
+
+	// TODO: dbl check this is an execution
+	var execution Execution
+	return &execution, json.NewDecoder(res.Body).Decode(&execution)
 }
 
 func (j *Jobs) urlEncodeListInput(rawURL string, input *ListJobsInput) (string, error) {
