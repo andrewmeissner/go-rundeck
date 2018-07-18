8174bf9181e22bfaea6d7ebd786762db9cc0e047
Andrew Meissner <andrew.meissner@careerbuilder.com>
2018-07-17 23:53:07 -0700

    importing and exporting jobs

--- jobs.go
+++ jobs.go
@@ -3,7 +3,9 @@ package rundeck
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -12,29 +14,37 @@ import (
 )
 
 const (
-	JobLogLevelDebug   = "DEBUG"
-	JobLogLevelVerbose = "VERBOSE"
-	JobLogLevelInfo    = "INFO"
-	JobLogLevelWarn    = "WARN"
-	JobLogLevelError   = "ERROR"
+	JobLogLevelDebug      = "DEBUG"
+	JobLogLevelVerbose    = "VERBOSE"
+	JobLogLevelInfo       = "INFO"
+	JobLogLevelWarn       = "WARN"
+	JobLogLevelError      = "ERROR"
+	JobFormatXML          = "xml"
+	JobFormatYAML         = "yaml"
+	DuplicateOptionSkip   = "skip"
+	DuplicateOptionCreate = "create"
+	DuplicateoptionUpdate = "update"
+	UUIDOptionPreserve    = "preserve"
+	UUIDOptionRemove      = "remove"
 )
 
 // Job is information about a Rundeck job
 type Job struct {
-	ID              string            `json:"id"`
-	AverageDuration int64             `json:"averageDuration"`
-	Name            string            `json:"name"`
-	Group           string            `json:"group"`
-	Project         string            `json:"project"`
-	Description     string            `json:"description"`
-	HREF            string            `json:"href"`
-	Permalink       string            `json:"permalink"`
-	Options         map[string]string `json:"options"`
-	Scheduled       bool              `json:"scheduled"`
-	ScheduleEnabled bool              `json:"scheduleEnabled"`
-	Enabled         bool              `json:"enabled"`
-	ServerNodeUUID  string            `json:"serverNodeUUID"`
-	ServerOwner     bool              `json:"serverOwner"`
+	ID              string            `json:"id,omitempty"`
+	AverageDuration int64             `json:"averageDuration,omitempty"`
+	Name            string            `json:"name,omitempty"`
+	Group           string            `json:"group,omitempty"`
+	Project         string            `json:"project,omitempty"`
+	Description     string            `json:"description,omitempty"`
+	HREF            string            `json:"href,omitempty"`
+	Permalink       string            `json:"permalink,omitempty"`
+	Options         map[string]string `json:"options,omitempty"`
+	Scheduled       bool              `json:"scheduled,omitempty"`
+	ScheduleEnabled bool              `json:"scheduleEnabled,omitempty"`
+	Enabled         bool              `json:"enabled,omitempty"`
+	ServerNodeUUID  string            `json:"serverNodeUUID,omitempty"`
+	ServerOwner     bool              `json:"serverOwner,omitempty"`
+	Index           int               `json:"index,omitempty"`
 }
 
 // ListJobsInput adds parameters to the endpoint for listing jobs
@@ -71,6 +81,29 @@ type RetryJobInput struct {
 	AsUser      string            `json:"asUser,omitempty"`
 	Options     map[string]string `json:"options,omitempty"`
 	FailedNodes bool              `json:"failedNodes,omitempty"`
+}
+
+// ExportJobsInput are the optional parameters for the export endpoint
+type ExportJobsInput struct {
+	Format    string
+	IDList    []string
+	GroupPath string
+	JobFilter string
+}
+
+// ImportJobsInput are the parameters for the import endpoint
+type ImportJobsInput struct {
+	FileFormat      string
+	DuplicateOption string
+	UUIDOption      string
+	RawContent      []byte
+}
+
+// ImportJobsResponse is the response that comes back from importing jobs
+type ImportJobsResponse struct {
+	Succeeded []*Job `json:"succeeded,omitempty"`
+	Failed    []*Job `json:"failed,omitempty"`
+	Skipped   []*Job `json:"skipped,omitempty"`
 }
 
 // Jobs is information pertaining to jobs API endpoints
@@ -158,6 +191,98 @@ func (j *Jobs) Retry(jobID string, execID int64, input *RetryJobInput) (*Executi
 	return &execution, json.NewDecoder(res.Body).Decode(&execution)
 }
 
+// Export exports a projects jobs defintions
+func (j *Jobs) Export(project string, input *ExportJobsInput) ([]byte, error) {
+	rawURL := j.c.RundeckAddr + "/project/" + project + "/jobs/export"
+
+	uri, err := url.Parse(rawURL)
+	if err != nil {
+		return nil, err
+	}
+
+	query := uri.Query()
+
+	if input != nil {
+		format := JobFormatXML
+		if strings.ToLower(input.Format) == JobFormatYAML {
+			format = JobFormatYAML
+		}
+		query.Add("format", format)
+
+		if input.GroupPath != "" {
+			query.Add("groupPath", input.GroupPath)
+		}
+
+		if input.IDList != nil && len(input.IDList) > 0 {
+			query.Add("idlist", strings.Join(input.IDList, ","))
+		}
+
+		if input.JobFilter != "" {
+			query.Add("jobFilter", input.JobFilter)
+		}
+	} else {
+		query.Add("format", JobFormatXML)
+	}
+
+	uri.RawQuery = query.Encode()
+
+	res, err := j.c.get(uri.String())
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, makeError(res.Body)
+	}
+
+	return ioutil.ReadAll(res.Body)
+}
+
+// Import imports job definitions
+func (j *Jobs) Import(project string, input *ImportJobsInput) (*ImportJobsResponse, error) {
+	if input == nil {
+		return nil, fmt.Errorf("input cannot be nil as ImportJobsInput.RawContent is required to import anything")
+	}
+	rawURL := j.c.RundeckAddr + "/project/" + project + "/jobs/import"
+
+	uri, err := url.Parse(rawURL)
+	if err != nil {
+		return nil, err
+	}
+
+	query := uri.Query()
+
+	fileFormat := JobFormatXML
+	if strings.ToLower(input.FileFormat) == JobFormatYAML {
+		fileFormat = JobFormatYAML
+	}
+	query.Add("fileformat", fileFormat)
+
+	if input.DuplicateOption != "" {
+		query.Add("dupeOption", input.DuplicateOption)
+	}
+
+	if input.UUIDOption != "" {
+		query.Add("uuidOption", input.UUIDOption)
+	}
+
+	uri.RawQuery = query.Encode()
+
+	res, err := j.c.post(uri.String(), bytes.NewReader(input.RawContent))
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, makeError(res.Body)
+	}
+
+	var resp ImportJobsResponse
+	return &resp, json.NewDecoder(res.Body).Decode(&resp)
+}
+
 func (j *Jobs) urlEncodeListInput(rawURL string, input *ListJobsInput) (string, error) {
 	uri, err := url.Parse(rawURL)
 	if err != nil {
@@ -196,7 +321,9 @@ func (j *Jobs) urlEncodeListInput(rawURL string, input *ListJobsInput) (string, 
 		}
 	}
 
-	return query.Encode(), nil
+	uri.RawQuery = query.Encode()
+
+	return uri.String(), nil
 }
 
 func (j *Jobs) convertToSerializeable(input *RunJobInput) *runJobInputSerializeable {
